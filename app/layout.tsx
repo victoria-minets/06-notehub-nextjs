aa95b93cc1a62578b5be2bab3a7061dc95d3ab2b
Victoria Minets <anni.nicht@gmail.com>
2025-10-02 20:28:50 +0300

    new register

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
-import TanstackProvider from '@/components/Lily/TanStackProvider';
+import TanstackProvider from '@/components/Lily/Lily';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 
